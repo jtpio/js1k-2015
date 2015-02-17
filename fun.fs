7f061892b06d63c95b1a419ca5e3470131d6fcd3
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-02-17 11:15:14 +0100

    1152

--- fun.fs
+++ fun.fs
@@ -11,16 +11,15 @@ float len(vec2 p)
 vec2 map(vec3 pos) {
     vec2 res = vec2(1.0);
     for (float i = 0.3; i < 1.0; i+=0.2) {
-        float s = sin(i * 28.0 + iGlobalTime / 3.0), c = cos(i * 28.0 + iGlobalTime / 3.0);
-        vec3 p = vec3(c*pos.x + s*pos.z, pos.y, -s*pos.x + c*pos.z) + vec3(2.0, sqrt(i) * 30.0 - 30.0, 1.0);
+        vec3 p = vec3(cos(i * 28.0 + iGlobalTime / 3.0)*pos.x + sin(i * 28.0 + iGlobalTime / 3.0)*pos.z, pos.y, -sin(i * 28.0 + iGlobalTime / 3.0)*pos.x + cos(i * 28.0 + iGlobalTime / 3.0)*pos.z) + vec3(2.0, sqrt(i) * 30.0 - 30.0, 1.0);
 
         // the train and the main tube
         // capped cylinder
-        vec2 d = abs(vec2(length(p.xy), p.z - 99.0 + mod(iGlobalTime * 20.0, 198.0))) - vec2(0.7, 5.0);
-        float cyl = min(max(d.x,d.y),0.0) + length(max(d,0.0));
+        vec2 d = abs(vec2(len(p.xy), p.z - 99.0 + mod(iGlobalTime / i * 20.0, 198.0))) - vec2(0.7, 5.0);
+        float cyl = min(max(d.x,d.y),0.0) + len(max(d,0.0));
 
         res = opU(res, opU(
-                opU(vec2(cyl, 50.0), vec2(length(p.yx)-0.5, 3.0)),
+                opU(vec2(cyl, 50.0), vec2(len(p.yx)-0.5, 3.0)),
                 // the torus along the tube
                 vec2(len(vec2(len(p.xy)-0.9,mod(p.z,5.0)-2.5))-0.2, 0.1))
               );
@@ -54,8 +53,8 @@ void main()
 
         vec3 lig = normalize(vec3(-0.6, 0.9, -0.5));
         float df = max(0.0, dot(nor, lig));
+        float sf = df;
         df = df < 0.1 ? 0.0 : (df < 0.3) ? 0.3 : (df < 0.7) ? 0.7 : 1.0;
-        float sf = max(0.0, dot(nor, lig));
 
         col = 0.4 * (1.0+sin(vec3(0.5,0.4,0)*(map(pos).y-1.0))) * (1.0 + df + step(0.3, sf*sf));
     }
--- fun_minified.h
+++ fun_minified.h
@@ -16,11 +16,10 @@ var fun_renamed_fs =
    "vec2 x=vec2(1.);" +
    "for(float f=.3;f<1.;f+=.2)" +
      "{" +
-       "float n=sin(f*28.+T/3.),m=cos(f*28.+T/3.);" +
-       "vec3 d=vec3(m*y.x+n*y.z,y.y,-n*y.x+m*y.z)+vec3(2.,sqrt(f)*30.-30.,1.);" +
-       "vec2 R=abs(vec2(length(d.xy),d.z-99.+mod(T*20.,198.)))-vec2(.7,5.);" +
-       "float l=min(max(R.x,R.y),0.)+length(max(R,0.));" +
-       "x=v(x,v(v(vec2(l,50.),vec2(length(d.yx)-.5,3.)),vec2(v(vec2(v(d.xy)-.9,mod(d.z,5.)-2.5))-.2,.1)));" +
+       "vec3 n=vec3(cos(f*28.+T/3.)*y.x+sin(f*28.+T/3.)*y.z,y.y,-sin(f*28.+T/3.)*y.x+cos(f*28.+T/3.)*y.z)+vec3(2.,sqrt(f)*30.-30.,1.);" +
+       "vec2 m=abs(vec2(v(n.xy),n.z-99.+mod(T/f*20.,198.)))-vec2(.7,5.);" +
+       "float z=min(max(m.x,m.y),0.)+v(max(m,0.));" +
+       "x=v(x,v(v(vec2(z,50.),vec2(v(n.yx)-.5,3.)),vec2(v(vec2(v(n.xy)-.9,mod(n.z,5.)-2.5))-.2,.1)));" +
      "}" +
    "return x;" +
  "}" +
@@ -30,22 +29,21 @@ var fun_renamed_fs =
    "v.x*=R.x/R.y;" +
    "vec3 x=vec3(0.,8.,-12.),y=normalize(vec3(v.xy,1.5));" +
    "float f=1.;" +
-   "for(int l=0;l<99;l++)" +
+   "for(int m=0;m<99;m++)" +
      "{" +
-       "float m=n(x+y*f).x;" +
-       "if(m<.01||f>60.)" +
+       "float z=n(x+y*f).x;" +
+       "if(z<.01||f>60.)" +
          "break;" +
-       "f+=m;" +
+       "f+=z;" +
      "}" +
-   "vec3 l=vec3(0.),m=x+f*y;" +
+   "vec3 m=vec3(0.),z=x+f*y;" +
    "if(f<60.)" +
      "{" +
-       "vec3 d=vec3(.01,0.,0.),R=normalize(vec3(n(m+d.xyy).x-n(m-d.xyy).x,n(m+d.yxy).x-n(m-d.yxy).x,n(m+d.yyx).x-n(m-d.yyx).x)),r=normalize(vec3(-.6,.9,-.5));" +
-       "float s=max(0.,dot(R,r));" +
-       "s=s<.1?0.:s<.3?.3:s<.7?.7:1.;" +
-       "float z=max(0.,dot(R,r));" +
-       "l=.4*(1.+sin(vec3(.5,.4,0)*(n(m).y-1.)))*(1.+s+step(.3,z*z));" +
+       "vec3 s=vec3(.01,0.,0.),c=normalize(vec3(n(z+s.xyy).x-n(z-s.xyy).x,n(z+s.yxy).x-n(z-s.yxy).x,n(z+s.yyx).x-n(z-s.yyx).x)),r=normalize(vec3(-.6,.9,-.5));" +
+       "float T=max(0.,dot(c,r)),d=T;" +
+       "T=T<.1?0.:T<.3?.3:T<.7?.7:1.;" +
+       "m=.4*(1.+sin(vec3(.5,.4,0)*(n(z).y-1.)))*(1.+T+step(.3,d*d));" +
      "}" +
-   "gl_FragColor=vec4(l,1.);" +
+   "gl_FragColor=vec4(m,1.);" +
  "}"
 
