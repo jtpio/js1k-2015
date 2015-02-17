bfd3ff8f24f7283df4ea5630ccbee6405bd7866d
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-02-17 23:51:13 +0100

    1103B

--- fun_minified.h
+++ fun_minified.h
@@ -7,39 +7,35 @@ var fun_renamed_fs =
  "{" +
    "return x.x<y.x?x:y;" +
  "}" +
- "float v(vec2 v)" +
- "{" +
-   "return v=v*v*v*v,pow(v.x+v.y,.25);" +
- "}" +
- "vec2 n(vec3 y)" +
+ "vec2 v(vec3 y)" +
  "{" +
    "vec2 x=vec2(1.);" +
-   "for(float m=.3;m<1.;m+=.1)" +
+   "for(float m=.3;m<1.;m+=.2)" +
      "{" +
-       "vec3 n=vec3(cos(m*99.+T/3.)*y.x+sin(m*99.+T/3.)*y.z,y.y,-sin(m*99.+T/3.)*y.x+cos(m*99.+T/3.)*y.z)+vec3(2.,sqrt(m)*40.-40.,1.);" +
-       "vec2 d=abs(vec2(v(n.xy),n.z-99.+mod(T/m*20.,198.)))-vec2(.7,5.);" +
-       "x=v(x,v(v(vec2(min(max(d.x,d.y),0.)+v(max(d,0.)),10.),vec2(v(n.yx)-.5,3.)),vec2(v(vec2(v(n.xy)-.9,mod(n.z,5.)-2.5))-.2,.1)));" +
+       "vec3 d=vec3(cos(m*9.+T*.3)*y.x+sin(m*9.+T*.3)*y.z,y.y,-sin(m*9.+T*.3)*y.x+cos(m*9.+T*.3)*y.z)+vec3(2.,sqrt(m)*20.-20.,1.);" +
+       "vec2 n=abs(vec2(length(d.xy),d.z-99.+mod(T/m*20.,198.)))-vec2(.7,5.);" +
+       "x=v(x,v(v(vec2(min(max(n.x,n.y),0.)+length(max(n,0.)),10.),vec2(length(d.yx)-.5,3.)),vec2(length(vec2(length(d.xy)-.9,mod(d.z,4.)-2.))-.2,.1)));" +
      "}" +
    "return x;" +
  "}" +
  "void main()" +
  "{" +
-   "vec2 v=-1.+2.*gl_FragCoord.xy/R.xy;" +
-   "v.x*=R.x/R.y;" +
-   "vec3 m=vec3(0.,8.,-16.),d=normalize(vec3(v.xy,2.));" +
-   "float x=1.;" +
-   "vec3 y=vec3(.9);" +
+   "vec2 m=-1.+2.*gl_FragCoord.xy/R.xy;" +
+   "m.x*=R.x/R.y;" +
+   "vec3 x=vec3(0.,4.4,-16.),T=normalize(vec3(m.xy,2.)),y;" +
+   "float d=1.,n;" +
+   "vec3 l=vec3(.1);" +
    "for(int f=0;f<99;f++)" +
      "{" +
-       "if(n(m+d*x).x<.01||x>60.)" +
+       "if((n=v(y=x+T*d).x)<.01||d>60.)" +
          "break;" +
-       "x+=n(m+d*x).x;" +
+       "d+=n;" +
      "}" +
-   "if(x<60.)" +
+   "if(d<60.)" +
      "{" +
-       "vec3 s=vec3(.01,0.,0.),z=normalize(vec3(n(m+d*x+s.xyy).x-n(m+d*x-s.xyy).x,n(m+d*x+s.yxy).x-n(m+d*x-s.yxy).x,n(m+d*x+s.yyx).x-n(m+d*x-s.yyx).x));" +
-       "y=.4*(1.+sin(vec3(.5,.4,0)*(n(m+d*x).y-1.)))*(1.+(max(0.,dot(z,normalize(vec3(-.6,.9,-.5))))<.1?0.:max(0.,dot(z,normalize(vec3(-.6,.9,-.5))))<.3?.3:max(0.,dot(z,normalize(vec3(-.6,.9,-.5))))<.7?.7:1.)+step(.3,max(0.,dot(z,normalize(vec3(-.6,.9,-.5))))*max(0.,dot(z,normalize(vec3(-.6,.9,-.5))))));" +
+       "vec3 s=vec3(.01,0.,0.),z=normalize(vec3(v(y+s.xyy).x-v(y-s.xyy).x,v(y+s.yxy).x-v(y-s.yxy).x,v(y+s.yyx).x-v(y-s.yyx).x));" +
+       "l=.4*(1.+sin(vec3(.5,.4,0)*(v(y).y-1.)))*(1.+(max(0.,dot(z,normalize(vec3(-.6,.9,-.5))))<.1?0.:max(0.,dot(z,normalize(vec3(-.6,.9,-.5))))<.3?.3:max(0.,dot(z,normalize(vec3(-.6,.9,-.5))))<.7?.7:1.)+step(.3,max(0.,dot(z,normalize(vec3(-.6,.9,-.5))))*max(0.,dot(z,normalize(vec3(-.6,.9,-.5))))));" +
      "}" +
-   "gl_FragColor=vec4(y,1.);" +
+   "gl_FragColor=vec4(l,1.);" +
  "}"
 
--- fun_renamed.fs
+++ fun_renamed.fs
@@ -2,54 +2,50 @@ vec2 opU( vec2 d1, vec2 d2 ) {
     return (d1.x<d2.x) ? d1 : d2;
 }
 
-float len(vec2 p)
-{
-    p = p*p*p*p;
-    return pow(p.x + p.y, 0.25);
-}
-
 vec2 map(vec3 pos) {
     vec2 res = vec2(1.0);
-    for (float i = 0.3; i < 1.0; i+=0.1) {
-        vec3 p = vec3(cos(i * 99.0 + T / 3.0)*pos.x + sin(i * 99.0 + T / 3.0)*pos.z, pos.y, -sin(i * 99.0 + T / 3.0)*pos.x + cos(i * 99.0 + T / 3.0)*pos.z) + vec3(2.0, sqrt(i) * 40.0 - 40.0, 1.0);
+    for (float i = 0.3; i < 1.0; i+=0.2) {
+
+        vec3 p = vec3(cos(i * 9.0 + T * 0.3)*pos.x + sin(i * 9.0 + T * 0.3)*pos.z, pos.y, -sin(i * 9.0 + T * 0.3)*pos.x + cos(i * 9.0 + T * 0.3)*pos.z) + vec3(2.0, sqrt(i) * 20.0 - 20.0, 1.0);
 
         // the train and the main tube
         // capped cylinder
-        vec2 d = abs(vec2(len(p.xy), p.z - 99.0 + mod(T / i * 20.0, 198.0))) - vec2(0.7, 5.0);
+        vec2 d = abs(vec2(length(p.xy), p.z - 99.0 + mod(T / i * 20.0, 198.0))) - vec2(0.7, 5.0);
 
         res = opU(res, opU(
-                opU(vec2(min(max(d.x,d.y),0.0) + len(max(d,0.0)), 10.0), vec2(len(p.yx)-0.5, 3.0)),
+                opU(vec2(min(max(d.x,d.y),0.0) + length(max(d,0.0)), 10.0), vec2(length(p.yx)-0.5, 3.0)),
                 // the torus along the tube
-                vec2(len(vec2(len(p.xy)-0.9,mod(p.z,5.0)-2.5))-0.2, 0.1))
+                vec2(length(vec2(length(p.xy)-0.9,mod(p.z,4.0)-2.0))-0.2, 0.1))
               );
     }
     return res;
 }
+
 
 void main()
 {
     vec2 p = (-1.0+2.0 * gl_FragCoord.xy/R.xy);
     p.x *= R.x/R.y;
 
-    vec3 ro = vec3(0.0, 8.0, -16.0);
-    vec3 rd = normalize(vec3(p.xy, 2.0));
+    vec3 ro = vec3(0.0, 4.4, -16.0);
+    vec3 rd = normalize(vec3(p.xy, 2.0)), pos;
 
-    float t = 1.0;
-    vec3 col = vec3(0.9);
+    float t = 1.0, res;
+    vec3 col = vec3(0.1);
 
     for (int i=0; i<99; i++) {
-        if(map(ro+rd*t).x < 0.01 || t > 60.0 ) break;
-        t += map(ro+rd*t).x;
+        if((res=map(pos=ro+rd*t).x) < 0.01 || t > 60.0 ) break;
+        t += res;
     }
 
     if (t < 60.0) {
         vec3 eps = vec3(0.01, 0.0, 0.0);
         vec3 nor = normalize(vec3(
-            map(ro+rd*t+eps.xyy).x - map(ro+rd*t-eps.xyy).x,
-            map(ro+rd*t+eps.yxy).x - map(ro+rd*t-eps.yxy).x,
-            map(ro+rd*t+eps.yyx).x - map(ro+rd*t-eps.yyx).x ));
+            map(pos+eps.xyy).x - map(pos-eps.xyy).x,
+            map(pos+eps.yxy).x - map(pos-eps.yxy).x,
+            map(pos+eps.yyx).x - map(pos-eps.yyx).x ));
 
-        col = 0.4 * (1.0+sin(vec3(0.5,0.4,0)*(map(ro+rd*t).y-1.0))) * (1.0 + (max(0.0, dot(nor, normalize(vec3(-0.6, 0.9, -0.5)))) < 0.1 ? 0.0 : max(0.0, dot(nor, normalize(vec3(-0.6, 0.9, -0.5)))) < 0.3 ? 0.3 : max(0.0, dot(nor, normalize(vec3(-0.6, 0.9, -0.5)))) < 0.7 ? 0.7 : 1.0) + step(0.3, max(0.0, dot(nor, normalize(vec3(-0.6, 0.9, -0.5))))*max(0.0, dot(nor, normalize(vec3(-0.6, 0.9, -0.5))))));
+        col = 0.4 * (1.0+sin(vec3(0.5,0.4,0)*(map(pos).y-1.0))) * (1.0 + (max(0.0, dot(nor, normalize(vec3(-0.6, 0.9, -0.5)))) < 0.1 ? 0.0 : max(0.0, dot(nor, normalize(vec3(-0.6, 0.9, -0.5)))) < 0.3 ? 0.3 : max(0.0, dot(nor, normalize(vec3(-0.6, 0.9, -0.5)))) < 0.7 ? 0.7 : 1.0) + step(0.3, max(0.0, dot(nor, normalize(vec3(-0.6, 0.9, -0.5))))*max(0.0, dot(nor, normalize(vec3(-0.6, 0.9, -0.5))))));
     }
 
     gl_FragColor = vec4(col/*mix(col, vec3(0.9), smoothstep(8.5,29.0, length(pos)))*/, 1.0);
